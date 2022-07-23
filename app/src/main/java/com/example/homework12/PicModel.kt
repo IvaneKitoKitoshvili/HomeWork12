d572e2bbfd651705be4a546808610641ab029891
Ivane Kito Kitoshvili <ivane.kitoshvili@gmail.com>
2022-07-23 22:43:38 +0400

    Add Visual components to project

--- app/src/main/java/com/example/homework12/PicModel.kt
+++ app/src/main/java/com/example/homework12/PicModel.kt
@@ -1,7 +1,11 @@
 package com.example.homework12
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class PicModel(
     val image: Int,
     val name: String,
     val description: String
-)
+):Parcelable
